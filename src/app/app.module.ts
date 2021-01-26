d586950db21febbfe6f4722fb4dc2be15a794436
sharsha2 <karuturiharsh@gmail.com>
2021-01-26 04:46:50 -0500

    Implemented register.component.html using reactive forms and performed validations and user can see the password entered by eye-slash function.
    differentiated forms login by template driven form and register by modern or reactive driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
 // tslint:disable-next-line:import-spacing
 
 
@@ -41,6 +42,7 @@ import { PageNotFoundComponent } from './components/shared/page-not-found/page-n
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup} from '@angular/forms';
+import { FormBuilder} from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+
+// tslint:disable-next-line:typedef
+function passwordMatchValidator(form) {
+  const password = form.get('password');
+  const confirmPassword = form.get('confirmPassword');
+
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.setErrors({passwordMatch: true});
+  }
+  else {
+    confirmPassword.setErrors(null);
+  }
+  return null;
+}
+
+// tslint:disable-next-line:typedef
+function symbolValidator(control) {
+  if (control.hasError('required')) {
+    return null;
+  }
+  if (control.hasError('minlength')) {
+    return null;
+  }
+  if (control.value.indexOf('@') > -1){
+    return null;
+  } else {
+    return { symbol: true};
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -6,10 +39,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  fieldTextType: boolean;
 
-  constructor() { }
+  constructor(private builder: FormBuilder) { }
 
-  ngOnInit(): void {
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  // tslint:disable-next-line:typedef
+  buildForm() {
+    this.registerForm = this.builder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, symbolValidator, Validators.minLength(8)]],
+      confirmPassword: '',
+    }, {
+      validators: passwordMatchValidator
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  register() {
+    console.log(this.registerForm.value);
+  }
+  // tslint:disable-next-line:typedef
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
   }
 
 }
